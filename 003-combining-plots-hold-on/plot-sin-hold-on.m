79f9d6aa3deaa09c7cb2c65b823b3a826ecd9a4d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-13 08:46:22 +0530

    add multiple plots

--- 003-combining-plots-hold-on/plot-sin-hold-on.m
+++ 003-combining-plots-hold-on/plot-sin-hold-on.m
@@ -3,6 +3,10 @@
 x = 0:0.2:10;
 %% y1 variable holds the sine of the variable x
 y1 = sin(x);
+
+y2 = sin(x/2)
+
+y3 = 2 * sin(x)
 %% finally, i will plot the values of variable x vs y1
 plot(x,y1)
 
@@ -15,8 +19,26 @@ hold on;
 %% hold on tells matlab not to create another figure
 
 %% now i want to plot x versus sin(x/2)
-y2 = sin(x/2)
 plot(x,y2)
+
+
+%% how to tell matlab not to add more plots to the same figure
+%% we use the reserved word
+%% hold off
+
+hold off;
+plot(x,y2);
+
+figure, plot(x,y2);
+
+%% for example, i want to display line plot and scatter plot together
+
+%% scatter plot is created using the built in function scatter
+scatter(x,y3)
+hold on;
+
+plot(x,y2)
+
 
 
 
