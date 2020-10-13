7c4e356aef441014da3255ff6ec2fd56e2a12bd3
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-13 11:22:56 +0530

    init

---
+++ 004-brush-tool-for-plots/brush-tool.m
@@ -0,0 +1,11 @@
+%% in this section we will be seeing a very intersting feature of matlab
+%% that is - we will be looking at how we can interact with data
+
+%% there are essentially many advantages of this - one such advantage of this 
+%% is to investigate the data for some possible outliers
+%% and then manually identify them and get rid of them
+
+%% let us do a quick example on this in order to illustrate the key concept 
+
+A = [67 59 60 100 59 58 300 61 62 60 62 58 57]
+
