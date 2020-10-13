7d36ed045a18bd40dbe258b46492077f851bd5fc
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-13 08:04:54 +0530

    plotting sine

---
+++ 003-combining-plots-hold-on/plot-sin.m
@@ -0,0 +1,9 @@
+%x contains 50 different values between 0 and 10 
+%% with a step size of 0.2 
+x = 0:0.2:10;
+%% y1 variable holds the sine of the variable x
+y1 = sin(x);
+%% finally, i will plot the values of variable x vs y1
+plot(x,y1)
+
+
