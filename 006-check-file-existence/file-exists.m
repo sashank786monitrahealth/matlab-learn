166367eb713c2eecb9002f573869d323bdc08d14
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-14 13:46:49 +0530

    check for sort function

---
+++ 006-check-file-existence/file-exists.m
@@ -0,0 +1,29 @@
+%% this topic covers different aspects of interacting with the matlab software
+%% In particular - we will be covering topics such as
+%% - manipulation of directories
+%%  - checking for existance of files, scripts folders 
+%%  - textual processing
+
+%% This means that we will now move from visual aspects 
+%% such as plots and graphs to topics that are more advanced 
+%% and essential for effective usage of matlab software.
+
+%% In this particular segment - we will be covering a function that is called exist.
+%% this function is used for checking of existance of variables
+%% scripts
+%% functions
+%% folders
+%% classes
+%% or files within the MatLab environment
+
+a = [1 2 3 4 5]
+exist a
+
+%% check for existance of html folder
+exist html
+
+%% checks for existance of file
+exist file-exists.m
+
+%%% check for existance of sort function
+exist sort
