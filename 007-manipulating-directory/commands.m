63210a0a660e50e46411f8a714ff3e97ef7172d8
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-15 21:44:18 +0530

    get information of first file

--- 007-manipulating-directory/commands.m
+++ 007-manipulating-directory/commands.m
@@ -17,3 +17,56 @@ dir **/*.m
 dir *f.m
 
 %% you may mention any regular expression with dir function
+
+% store the results of the function dir in a variable
+y = dir
+
+% octave:3> y
+% y =
+% 
+%   2x1 struct array containing the fields:
+% 
+%     name
+%     folder
+%     date
+%     bytes
+%     isdir
+%     datenum
+%     statinfo
+
+% octave:4> 
+
+% it returns a struct array
+
+
+
+octave:3> y(1)
+ans =
+
+  scalar structure containing the fields:
+
+    name = .
+    folder = /home/oo
+    date = 15-Oct-2020 16:12:06
+    bytes = 1024
+    isdir = 1
+    datenum = 7.3808e+05
+    statinfo =
+
+      scalar structure containing the fields:
+
+        dev = 1851
+        ino = 1833
+        mode = 16832
+        modestr = drwx------ 
+        nlink = 2
+        uid = 1500
+        gid = 1500
+        rdev = 0
+        size = 1024
+        atime = 1.6028e+09
+        mtime = 1.6028e+09
+        ctime = 1.6028e+09
+        blksize = 1024
+        blocks = 4
+
