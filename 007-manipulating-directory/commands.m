b2fb1750f7db1a7b121e3d2a83a1024de9038c9b
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-15 21:10:54 +0530

    search through all folders and their subfolders

--- 007-manipulating-directory/commands.m
+++ 007-manipulating-directory/commands.m
@@ -4,4 +4,13 @@ dir
 % search for patterns in files in directory
 dir *.m
 
+%list files in any directory under the current working directory
+dir */*.m
+
+% if we want to find files in all sub-folders and their subfolders down to the very 
+% ending level of the sub-folders
+% then we need to put two stars before the backslash 
+
+dir **/.m
+
 
