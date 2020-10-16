59c97524862e7c3d1dd929d5dd4c67967acf3f12
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-16 07:01:33 +0530

    mkdir rmdir

--- 008-manipulating-directory-2/commands.m
+++ 008-manipulating-directory-2/commands.m
@@ -1,2 +1,16 @@
 % here we will build upon previous lessons
 
+%% how to search files which are not in our current working directory
+%% that is independently of our cwd
+%% cd - change directory
+cd ../
+
+%% this will move one level up
+dir
+
+%% create directory 
+mkdir matlabWorks
+
+cd matlabWorks
+rmdir matlabWorks
+
