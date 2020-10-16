6cfa4c19644609a3e6f64930d9bc88ae9a6b9642
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-16 07:47:52 +0530

    using string function to convert to string type

--- 009-processing-text-files/commands.m
+++ 009-processing-text-files/commands.m
@@ -4,4 +4,18 @@
 % single argument it needs is the location of text files
 fileread('/home/ubuntu/1.txt')
 
+% read into a variable
+myText = fileread('/home/ubuntu/1.txt')
+
+% whos command will return all the variables in our workspace
+whos
+
+% in order to work on the contents of the file we will want to
+% convert its type into string
+% because it is very difficult to work characters
+
+% the string function will be used for this purpose 
+
+myText = string(myText);
+
 
