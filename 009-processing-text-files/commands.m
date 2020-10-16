52d33c9659ea1934b547fbc947b2f05c0cc10f75
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-16 07:37:51 +0530

    fileread command to read files into matlab environment

---
+++ 009-processing-text-files/commands.m
@@ -0,0 +1,7 @@
+% the function that we will need to use for reading text files 
+% into matlab environment is the fileread function
+
+% single argument it needs is the location of text files
+fileread('')
+
+
