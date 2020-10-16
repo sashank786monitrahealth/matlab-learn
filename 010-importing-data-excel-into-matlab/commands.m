65533a67fa7c03fe0e288f41a006dbdfd764c1ea
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-16 21:15:54 +0530

    describing the import tool

---
+++ 010-importing-data-excel-into-matlab/commands.m
@@ -0,0 +1,25 @@
+%% in this section we will be looking at how we can import data into the matlab environment
+%% so that we can take advantage of Matlab enabled data analysis and perform different operations
+%% such as data visualizations on the data.
+
+%% Moreover we will also learn about one of the tools of Matlab called a spreadsheet link - which allows
+%% you to connect to matlab with the Microsoft Excel software and give you a kind of 
+%% Matlab version of the excel environment.
+
+%% This means that you can take advantage of the MATLAB software while using the famous Microsoft Excel
+%% program. So with this brief introduction of the section let us begin this tutorial.
+
+%% In this first tutorial of the section we will be learning how to import data into the MATLAB
+%% that is being provided to us within the excel sheet. The matlab provides us with an interesting 
+%% feature and that is called the import data feature.
+
+
+%% When we click it a browser will pop up for selecting file. On selecting a file another popup will
+%% come up. This popup is basically the import data tool and you can use it to bring your data
+%% into the MATLAB environment.
+
+
+
+
+
+
