d98e677c1d0c758d96c8c902b8b88090fe81efa7
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-17 11:14:51 +0530

    passing data to excel from matlab

---
+++ 014-passing-data-excel-matlab/commands.m
@@ -0,0 +1,6 @@
+%% we have successfully installed spreadsheet link in last session
+%% set excel matlab plugin preferences
+%% select -- right click on the selected cells -- send to matlab 
+%% set a variable name which will hold this in matlab environment
+%% get data from matlab -- right click -- and choose the variable to import from
+%% 
