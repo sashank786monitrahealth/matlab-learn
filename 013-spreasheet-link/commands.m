2c37d8361275e652c74bd1689b65dc5a7a5a4447
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-17 08:46:45 +0530

    passing data between excel and matlab

---
+++ 013-spreasheet-link/commands.m
@@ -0,0 +1,22 @@
+%% spreadsheet link helps matlab to be integrated with microsoft excel software
+%% Basic motivation / Benefit of the Spreadsheet link feature is that for those
+%% users who are addicted to the excel environment and they are using 
+%% excel for a very long time.
+
+%% And therefore they cannot suddenly leave that environment. 
+
+%% So this program enables them to use and take advantage of the matlab functions
+%% while still being in the excel environment. 
+
+%% Basically, because of human psychology that we as humans resist change. 
+%% And resist new things in the beginning as there are a lot of adjustments 
+%% that we need to make in our daily life. 
+
+%% So for those users who have taken years to familiarize themselves with the excel. Now moving into a 
+%% totally new environment maybe a bit hard for them. 
+
+%% So for those users, the spreadsheet link has been introduced. 
+
+%% We will show how to setup spreadsheet link in our excel environment. 
+
+%% 
