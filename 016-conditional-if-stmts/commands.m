ca8ec03a2d0757660a56c15b4d827da55aa0ac19
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-18 10:08:40 +0530

    student marks

---
+++ 016-conditional-if-stmts/commands.m
@@ -0,0 +1,6 @@
+%% in general there can be more than one else if
+
+%% determine the grade of a student based on his marks that he has obtained in his course
+
+marks = input("Please enter your marks");
+
