97af7024cbdbd566db47363a34463e40663e9154
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-18 10:35:05 +0530

    Studen Grade

--- 016-conditional-if-stmts/commands.m
+++ 016-conditional-if-stmts/commands.m
@@ -3,4 +3,23 @@
 %% determine the grade of a student based on his marks that he has obtained in his course
 
 marks = input("Please enter your marks");
+84
+
+
+if marks >=90
+   Grade = string('A');
+elseif marks >= 80 && marks < 90
+   Grade = string('B');
+elseif marks >= 70 && marks < 80
+   Grade = string('C');
+elseif marks >= 60 && marks < 70
+   Grade = string('D');
+else
+   Grade = string('F');
+   
+end;
+
+disp("Your Grade is")
+disp(Grade)
+
 
