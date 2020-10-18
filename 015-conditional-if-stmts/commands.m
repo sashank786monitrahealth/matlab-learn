8e24f31a664fd91e89067d2a476715299008d914
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-18 07:44:38 +0530

    definitions

---
+++ 015-conditional-if-stmts/commands.m
@@ -0,0 +1,16 @@
+%% This is brand new section of the course. This section is quite different from the previous sections.
+% In the sense, that in this section we will not be merely be explaining the built in functions
+% In all of the previous sections we were talking about the use of built-in functions
+% for performing differnt tasks 
+%% Now, since the tasks can be quite different. 
+% Quite large in number and may significantly vary from one domain to another 
+%% For instance, we may be interested in different set of tasks
+% when we are performing signal processing. And we may be interested in a separate list of tasks
+%% when we are engaged in image processing.
+
+%% So generally speaking it is not possible for the implementers of the software to list down
+%% all possible set of tasks and problems and write built in functions
+%% 
+%% So - in summary - sooner or later you will encounter problems for which you will not be able to find
+%% a built in function - that will give you the solution. And, therefore you will have to write your own code and 
+%% function to accomplish that. 
