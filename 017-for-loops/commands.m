7902d31f95376c029c1864dce7299edb7379ecd3
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-19 09:03:22 +0530

    examples of for loop

---
+++ 017-for-loops/commands.m
@@ -0,0 +1,34 @@
+%for loop
+% most important and fundamental statement for controlling the execution flow within 
+% the program
+%%
+%%
+%%
+%% By execution flow - order in which the statements are going to be executed
+%%
+%% For loop basically allows us to specify iterations. In particular, it allows 
+%% a certain piece of code to be executed repeatedly. Since, we will be mostly be writing codes
+%% which contains many statements - hence, we will include them in a script. 
+%%
+%% Scripts help us to organize our code and we can use them in situations when our
+%% code grows in a number of statements. 
+%% 
+%% Essentially these are the same statements that we can run on the command line
+%% But, the scripts help us to organize them in a more meaningful way
+%% 
+%% The basic structure for including a for loop is by mentioning the reserve word for now.
+
+for i=1:2:10
+ disp(i)
+end
+
+
+for i=10:-2:0
+ disp(i)
+end
+
+
+for i=10:-1:0
+ disp(i)
+end
+
