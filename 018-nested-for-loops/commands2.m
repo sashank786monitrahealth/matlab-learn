e7994758795142f4d5ae005867dba7876176d52d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-22 08:40:50 +0530

    get mean of grades of 10 students

--- 018-nested-for-loops/commands2.m
+++ 018-nested-for-loops/commands2.m
@@ -1,1 +1,13 @@
-fprintf("Please enter 10 student Grades in the form of percentages\n");
+fprintf("Please enter 5 student Grades in the current semester\n");
+
+for i = 1:5
+   fprintf("Please enter the grades for the student number %d",i);
+   for j=1:5
+      fprintf("Please enter the grade percentage in subject %d for student %d\n",j,i);
+      grades(i,j) = input('');
+   end
+end
+
+fprintf('The average grade is %d', mean(grades));
+
+
