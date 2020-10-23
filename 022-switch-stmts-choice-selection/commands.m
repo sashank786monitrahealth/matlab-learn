07d2b21a6abed7e4feb69efcac59f38fa5a5be15
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-23 09:57:22 +0530

    switch init

---
+++ 022-switch-stmts-choice-selection/commands.m
@@ -0,0 +1,18 @@
+clear all;
+
+% the switch statement is used for making a selection
+
+m = input('Enter a number:');
+
+%  switch m
+%     case
+%     case
+%     case
+%     otherwise
+%  end
+
+
+
+
+
+
--- 022-switch-stmts-choice-selection/readme.md
+++ 022-switch-stmts-choice-selection/readme.md
@@ -5,3 +5,12 @@ Learning how to make choices with switch statement
  - Choices in numeric form
  - Choices in character form
 
+
+switch format:
+
+switch m
+   case
+   case
+   case
+   otherwise
+end
