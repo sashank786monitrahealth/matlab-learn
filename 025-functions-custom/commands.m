186f43f055147270265fe76b24ef608d33225e3e
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-23 12:26:03 +0530

    add custom function code

---
+++ 025-functions-custom/commands.m
@@ -0,0 +1,27 @@
+clear all;
+
+% the switch statement is used for making a selection
+
+%  switch m
+%     case
+%     case
+%     case
+%     otherwise
+%  end
+
+
+m = input('Enter a number:');
+
+switch m
+  case 1
+     disp('The number is a positive one')
+  case 0
+     disp('The number is a zero')
+  otherwise
+     disp('I dont know') 
+end
+
+
+
+
+
---
+++ 025-functions-custom/readme.md
@@ -0,0 +1,6 @@
+Learning Objectives:
+=======================
+
+Learning custom built functions
+- Custom built functions with no inputs and outputs
+- A simple print function
