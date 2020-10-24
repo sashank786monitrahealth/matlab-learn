5bee96287d66996f63b48015b270fac555bbb79c
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-24 10:39:40 +0530

    matrices - intro

--- 027-functions-with-inputs-outputs/commands.m
+++ 027-functions-with-inputs-outputs/commands.m
@@ -1,2 +1,22 @@
 %% In this tutorial we will looking at functions with multiple inputs and outputs
+%% in order to mention the outputs of a function we need to use square brackets []
+%% after the reserved word of function
+%% and mentione them in the square brackets
+%% However, if the output is only one. Then we do not need to mention it in the square brackets
+%% We just mention the output variable name without the brackets 
+%% just after the reserved word of function. 
+
+%%
+%% Let me define the purpose of the function
+%% The function is going to concatenate two matrices together and 
+%% and then the function is going to compute different statistics 
+%% such as the standard deviation 
+%% variance
+%% minimum value
+%% maximum value of the matrices
+%% and they will be returned as output of the function.
+
+function [] = statistics_of_data(a,b)
+   
+end
 
