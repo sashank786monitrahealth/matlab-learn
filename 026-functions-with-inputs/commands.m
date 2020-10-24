871077332f15a06fc31c46760e65cb5cff8b0794
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-24 09:21:33 +0530

    square of a function

--- 026-functions-with-inputs/commands.m
+++ 026-functions-with-inputs/commands.m
@@ -1,4 +1,11 @@
 %% In this tutorial we will be going a step ahead and look at functions
 %% which contain inputs - this means that our functions will now be a bit more 
 %% sophisticated
-%% 
+%% So lets dig out - how we can create functions with inputs.
+
+
+function square(a)
+   fprintf("The square of the input provided is %d\n", a*a)
+end
+
+
