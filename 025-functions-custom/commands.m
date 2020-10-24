b6534501d48038db978d0a27e999c3d138b4bb8a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-24 09:09:51 +0530

    simplest matlab function

--- 025-functions-custom/commands.m
+++ 025-functions-custom/commands.m
@@ -4,4 +4,9 @@
 %% the outputs of a functions are being written before its name at the left hand side of the function
 %% so that was the basic syntax of the function use.
 
+%% now depending on the input and outputs - there can be many different types of functions 
 
+% simplest function 
+function print_function()
+  disp("Hello, this is the starting function")
+end
