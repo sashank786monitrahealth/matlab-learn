d1a0afa53db22ee615b219699735b3cbaefc888b
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-24 09:06:02 +0530

    simplest function

--- 025-functions-custom/commands.m
+++ 025-functions-custom/commands.m
@@ -1,6 +1,7 @@
 %% add custom function code
 %% the common thing in all these functions is that we will provide some inputs
 %% to the functions after writing its name in smooth brackets
-
+%% the outputs of a functions are being written before its name at the left hand side of the function
+%% so that was the basic syntax of the function use.
 
 
