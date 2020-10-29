330e0e284099607ea22caacc197e236181e79184
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-29 22:04:59 +0530

    cell datatype

---
+++ 032-creating-cells/commands.m
@@ -0,0 +1,11 @@
+%%This is going to be a short section where we will be learning how we can
+%% generate reports for sharing our code. There can be several motivations for this
+%% Such as I may want to share my code or results to my students or employers.
+%% Or I just want to share them with my friends, research partners or colleagues.
+%% 
+%% The matlab provides very good functionality in this regard. 
+%% 
+%% Publish Tab
+%% But, before we want to publish our results we would like to divide our code into meaningful portions.
+%%
+%% 
---
+++ 032-creating-cells/readme.md
@@ -0,0 +1,7 @@
+Learning Objectives:
+=======================
+
+Learning basics of cells data type
+ - Creating cell
+ - Visualizing cell
+ - Accessing locations of a cell
